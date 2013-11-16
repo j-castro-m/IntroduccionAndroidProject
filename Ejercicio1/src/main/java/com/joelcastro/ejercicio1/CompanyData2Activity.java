8b8c9aec31852cf4c8d6a14a77372eeccb0d5942
vlad29 <vladevil@gmail.com>
2013-11-16 09:16:11 +0100

    Version 0.1

--- Ejercicio1/src/main/java/com/joelcastro/ejercicio1/CompanyData2Activity.java
+++ Ejercicio1/src/main/java/com/joelcastro/ejercicio1/CompanyData2Activity.java
@@ -17,49 +17,5 @@ public class CompanyData2Activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_companydata2);
-
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.company_data2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_companydata2, container, false);
-            return rootView;
-        }
-    }
-
 }
--- Ejercicio1/src/main/java/com/joelcastro/ejercicio1/UserTypeActivity.java
+++ Ejercicio1/src/main/java/com/joelcastro/ejercicio1/UserTypeActivity.java
@@ -3,6 +3,7 @@ package com.joelcastro.ejercicio1;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class UserTypeActivity extends Activity {
 
@@ -18,49 +21,24 @@ public class UserTypeActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usertype);
 
-        if (savedInstanceState == null) {
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-    }
+        final Button button = (Button) findViewById(R.id.button_deposito);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
+                if(((RadioButton) findViewById(R.id.rbuttonCompany)).isChecked())
+                {
+                    Intent intent = new Intent(UserTypeActivity.this, CompanyData1Activity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Intent intent = new Intent(UserTypeActivity.this, resultActivity.class);
+                    startActivity(intent);
+                }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.user_type, menu);
-        return true;
-    }
+            }
+        });
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_usertype, container, false);
-            //test
-            return rootView;
-        }
     }
 
 }
